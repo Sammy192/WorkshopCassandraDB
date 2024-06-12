31888e266bcf3b42b069693d6f57a151393d6561
Samuel R Borges <sammy_rb@hotmail.com>
2024-06-12 16:48:20 -0300

    GET department by id and exception handling created

--- src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
@@ -3,10 +3,13 @@ package com.devsuperior.workshopcassandra.services;
 import com.devsuperior.workshopcassandra.model.dto.DepartmentDTO;
 import com.devsuperior.workshopcassandra.model.entities.Department;
 import com.devsuperior.workshopcassandra.repositories.DepartmentRepository;
+import com.devsuperior.workshopcassandra.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,5 +21,11 @@ public class DepartmentService {
     public List<DepartmentDTO> findAll() {
         List<Department> list = repository.findAll();
         return list.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+    }
+
+    public DepartmentDTO findById(UUID id) {
+        Optional<Department> result = repository.findById(id);
+        Department entity = result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+        return new DepartmentDTO(entity);
     }
 }
