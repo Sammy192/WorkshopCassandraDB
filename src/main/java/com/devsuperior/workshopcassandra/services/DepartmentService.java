2710155bb2c5723ce06330204d6c9a7f4aa7535d
Samuel R Borges <sammy_rb@hotmail.com>
2024-06-13 17:35:51 -0300

    PUT department created

--- src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
+++ src/main/java/com/devsuperior/workshopcassandra/services/DepartmentService.java
@@ -24,9 +24,13 @@ public class DepartmentService {
     }
 
     public DepartmentDTO findById(UUID id) {
-        Optional<Department> result = repository.findById(id);
-        Department entity = result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
+        Department entity = getById(id);
         return new DepartmentDTO(entity);
+    }
+
+    private Department getById(UUID id) {
+        Optional<Department> result = repository.findById(id);
+        return result.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado"));
     }
 
     public DepartmentDTO insert(DepartmentDTO dto) {
@@ -36,6 +40,14 @@ public class DepartmentService {
         entity = repository.save(entity);
         return new DepartmentDTO(entity);
     }
+
+    public DepartmentDTO update(UUID id, DepartmentDTO dto) {
+        Department entity = getById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new DepartmentDTO(entity);
+    }
+
 
     private void copyDtoToEntity(DepartmentDTO dto, Department entity) {
         entity.setName(dto.getName());
